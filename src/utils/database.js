2c04662ac3b45ca99967e7ccf6704e91f90f7f9e
Lucas Murakami <lkenjim98@gmail.com>
2021-01-26 20:49:50 -0300

    app initial structure

---
+++ src/utils/database.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+});
+
+module.exports = sequelize;
