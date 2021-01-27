daf24480132cb19e56cbdc4b731ed7dd5e4f667d
Lucas Murakami <lkenjim98@gmail.com>
2021-01-26 23:27:07 -0300

    Creating user's session in database with its token

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,12 +1,18 @@
+const uuid = require('uuid');
+
 const User = require('../models/User');
+const Session = require('../models/Session');
 const sanitize = require('../utils/sanitizer');
 
 class UsersController {
-    createUser(name) {
+    async createUser(name) {
         [name] = sanitize([name]);
         name = name.replace(name[0], name[0].toUpperCase());
+        
+        const newUser = await User.create({ name });
+        await Session.create({ userId: newUser.id, token: uuid.v4() });
 
-        return User.create({ name });
+        return newUser;
     }
 }
 
