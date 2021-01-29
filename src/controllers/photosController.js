588216121f6214c56c5f073e8d8a25ffc661f2a7
Lucas Murakami <lkenjim98@gmail.com>
2021-01-28 23:52:40 -0300

    POST photo likes route

--- src/controllers/photosController.js
+++ src/controllers/photosController.js
@@ -1,4 +1,16 @@
+const Photo = require('../models/Photo');
+
 class PhotosController {
+    async postLike(id) {
+        const photo = await Photo.findByPk(id);
+
+        if (photo) {
+            photo.likes = photo.likes + 1;
+            
+            return await photo.save();
+        }
+        else return await Photo.create({ id });
+    }
 }
 
 module.exports = new PhotosController();
--- src/routers/photosRouter.js
+++ src/routers/photosRouter.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
-router.post('/:id/likes', () => {
-    
+const photosController = require('../controllers/photosController');
+
+router.post('/:id/likes', async (req, res) => {
+    const id = parseInt(req.params.id);
+
+    try {
+        const newPhoto = await photosController.postLike(id);
+
+        res.send(newPhoto);
+    }
+    catch(err) {
+        console.error(err);
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
