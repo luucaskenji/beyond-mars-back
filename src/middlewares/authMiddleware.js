0789fa4b2b3ac30a69bd158f8c66012abf3b2cf2
Lucas Murakami <lkenjim98@gmail.com>
2021-01-30 15:22:42 -0300

    Verifying if token is valid in middleware

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,5 +1,8 @@
+const sessionsController = require('../controllers/sessionsController');
+
 const authMiddleware = async (req, res, next) => {
-    console.log(req.cookies);
+    const session = await sessionsController.findByToken(req.cookies.token);
+    if (!session) return res.status(403).send('Invalid token');
 
     next();
 };
